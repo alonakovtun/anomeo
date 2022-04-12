b108f30fb8aa38fc597c4efba7f97c65c74cef75
alonakovtun <alonakovtun55@gmail.com>
2022-04-12 10:13:36 +0200

    single blog

--- wp-content/themes/anomeo/template-parts/content-single-post.php
+++ wp-content/themes/anomeo/template-parts/content-single-post.php
@@ -10,63 +10,60 @@
 
 ?>
 
-<div class="ak-page__title-row">
-    <h1 class="title"><span><?= get_the_title(); ?></span> <span class="date-published"><?= get_the_date('d.m.Y'); ?></span></h1>
-</div>
-
 <? if (get_the_content()) : ?>
-    <section class="post-content__section post-content__section--text">
-        <div class="text-box text-box--left">
 
-        </div>
-        <div class="text-box text-box--right">
-            <?= get_the_content(); ?>
-        </div>
-    </section>
     <section class="post-content__section post-content__section--one_image_horizontal">
         <? the_post_thumbnail('', ['class' => 'post-content__image']); ?>
     </section>
+
+    <section class="post-content__section post-content__section--text">
+
+        <div class="text-box text-box--left">
+            <a href="/blog/"><? _e('Back to list', 'anomeo'); ?></a>
+            <p>category name</p>
+        </div>
+        <div class="text-box text-box--right">
+            <a class="post-date"><?php the_field('date'); ?></a>
+            <p class="post-title"><? the_title() ?></p>
+            <?php the_field('post_content1'); ?>
+
+
+        </div>
+
+    </section>
+    <div class="post-content-img">
+        <?php
+        $post_image1 = get_field('post_image1');
+        $post_image2 = get_field('post_image2');
+        if ((!empty($post_image1)) || (!empty($post_image2))) : ?>
+            <img src="<?php echo esc_url($post_image1); ?>" />
+            <img src="<?php echo esc_url($post_image2); ?>" />
+
+        <?php endif; ?>
+    </div>
+    <section class="post-content__section post-content__section--text">
+
+        <div class="text-box text-box--left">
+        </div>
+        <div class="text-box text-box--right">
+            <?php the_field('post_content2'); ?>
+        </div>
+    </section>
+    <?php
+    $image = get_field('footer_post_image');
+    if (!empty($image)) : ?>
+        <img src="<?php echo esc_url($image); ?>" />
+    <?php endif; ?>
+
 <? endif; ?>
 
-<? if (have_rows('sections_repeater')) :
-    while (have_rows('sections_repeater')) : the_row();
-        $section_type = get_sub_field('section_type');
-?>
-        <section class="post-content__section post-content__section--<?= $section_type; ?>">
-            <? if ($section_type == 'text') : ?>
-                <div class="text-box text-box--left">
-                    <?= get_sub_field('text_left'); ?>
-                </div>
-                <div class="text-box text-box--right">
-                    <?= get_sub_field('text_right'); ?>
-                </div>
-            <? endif; ?>
 
-            <? if ($section_type == 'one_image_horizontal' || $section_type == 'one_image_vertical') : ?>
-                <img src="<? the_sub_field('first_image'); ?>" class="post-content__image" />
-            <? endif; ?>
-
-            <? if ($section_type == 'two_images_vertical') : ?>
-                <div class="image-box">
-                    <div class="post-content__image-wrap">
-                        <img src="<? the_sub_field('first_image'); ?>" class="post-content__image" />
-                    </div>
-                </div>
-                <div class="image-box">
-                    <div class="post-content__image-wrap">
-                        <img src="<? the_sub_field('second_image'); ?>" class="post-content__image" />
-                    </div>
-                </div>
-            <? endif; ?>
-        </section>
-<? endwhile;
-endif; ?>
 <?
 $related_posts = get_posts(array('category__in' => wp_get_post_categories($post->ID), 'numberposts' => 2, 'post__not_in' => array($post->ID)));
 
 if ($related_posts) : ?>
     <div class="related-posts">
-        <p class="section-title"><? _e('Featured posts', 'anomeo'); ?></p>
+        <p class="section-title"><? _e('Related arcticles', 'anomeo'); ?></p>
 
         <div class="related-posts__grid">
             <? foreach ($related_posts as $rel_post) {
